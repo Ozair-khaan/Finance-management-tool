33cfb9ea25083f271cac2bed66bad719c43a93d1
Ozair <ozairkhantech@gmail.com>
2025-01-26 10:15:43 +0530

    removed config error and logging

--- src/main/java/com/finance/FinanceManagementToolApplication.java
+++ src/main/java/com/finance/FinanceManagementToolApplication.java
@@ -1,13 +1,27 @@
 package com.finance;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
-public class FinanceManagementToolApplication {
+public class FinanceManagementToolApplication implements CommandLineRunner {
 
-	public static void main(String[] args) {
-		SpringApplication.run(FinanceManagementToolApplication.class, args);
-	}
+    private final ApplicationContext context;
 
+    public FinanceManagementToolApplication(ApplicationContext context) {
+        this.context = context;
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(FinanceManagementToolApplication.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        String port = context.getEnvironment().getProperty("local.server.port");
+        System.out.println("Tomcat started on port : " + port);
+    }
 }
